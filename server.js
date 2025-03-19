02d222d055a741d27d7ca08d6a7f10dcafff8e47
Gayatri <gayatrisingh2300@gmail.com>
2025-03-20 03:59:20 +0530

    create server using http module

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http= require("node:http");
 const server = http.createServer(function(req, res) {
     if(req.url === "/getSecretData") {
         res.end("There is no secret data")
+        return;
     }
     res.end("Hello World")
 });
